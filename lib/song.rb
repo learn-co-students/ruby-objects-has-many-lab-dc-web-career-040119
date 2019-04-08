2565e4dc136edb69d88140f9dc32bf020b1a61a3
Matthew Lindsey <mbl@null.net>
2019-04-08 16:12:30 -0400

    done

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @artist = nil
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+end
